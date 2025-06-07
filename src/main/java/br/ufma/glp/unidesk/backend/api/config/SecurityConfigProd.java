ba32fd3d68c48cb3dc9aac9ca595411fc7b8f450
Renef <renefricardo30@gmail.com>
2025-06-07 17:46:58 -0300

    Adicionar configuração de segurança com JWT e CORS para a aplicação UniDesk

---
+++ src/main/java/br/ufma/glp/unidesk/backend/api/config/SecurityConfigProd.java
@@ -0,0 +1,35 @@
+package br.ufma.glp.unidesk.backend.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("prod")
+public class SecurityConfigProd {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET,"/v1/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/v1/usuarios").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/v1/auth/login").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/api-docs/**","/swagger-ui/**","/v3/api-docs/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
