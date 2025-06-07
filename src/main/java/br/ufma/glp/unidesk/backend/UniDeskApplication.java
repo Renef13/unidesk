f85572850df66e832476956579096d606ebc6024
Renef <renefricardo30@gmail.com>
2025-06-07 15:24:16 -0300

    Inicializar a aplicação UniDesk com configuração do Gradle e configurações básicas

---
+++ src/main/java/br/ufma/glp/unidesk/backend/UniDeskApplication.java
@@ -0,0 +1,13 @@
+package br.ufma.glp.unidesk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufma/glp/unidesk/backend/UniDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufma.glp.unidesk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
