5ea908f09546740e67206e40c2b949fb9a71a5ac
Renef <renefricardo30@gmail.com>
2025-07-15 11:23:36 -0300

    Refatorar classes Aluno e Usuario para incluir validações e herança

--- src/main/java/br/ufma/glp/unidesk/backend/domain/model/Aluno.java
+++ src/main/java/br/ufma/glp/unidesk/backend/domain/model/Aluno.java
@@ -1,28 +1,25 @@
 package br.ufma.glp.unidesk.backend.domain.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 @AllArgsConstructor
-public class Aluno {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idAluno;
+@Table(name = "alunos")
+public class Aluno extends Usuario{
 
     @Column(name = "matricula", unique = true)
-    @NotEmpty(message = "Matrícula não pode ser vazia")
+    @NotBlank(message = "Matrícula não pode ser vazia ou conter apenas espaços em branco")
     private String matricula;
 
     @ManyToOne
+    @NotNull(message = "Curso não pode ser nulo")
     @JoinColumn(name = "id_curso")
     private Curso curso;
 
-    @ManyToOne
-    @JoinColumn(name = "id_usuario")
-    private Usuario usuario;
 }
