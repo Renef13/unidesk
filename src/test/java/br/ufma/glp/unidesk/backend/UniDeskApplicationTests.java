a4dcabe78d64509ee1ea40a1c368852611135235
Renef <renefricardo30@gmail.com>
2025-07-15 13:32:59 -0300

    Comentar método de teste contextLoads na classe UniDeskApplicationTests

--- src/test/java/br/ufma/glp/unidesk/backend/UniDeskApplicationTests.java
+++ src/test/java/br/ufma/glp/unidesk/backend/UniDeskApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UniDeskApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
