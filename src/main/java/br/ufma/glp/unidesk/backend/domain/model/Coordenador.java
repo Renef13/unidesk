e641506197f91de8e50b7dd6c1fc46a2e71fe528
Renef <renefricardo30@gmail.com>
2025-07-15 11:29:46 -0300

    Refatorar classes Aluno e Usuario para incluir validações e herança

--- src/main/java/br/ufma/glp/unidesk/backend/domain/model/Coordenador.java
+++ src/main/java/br/ufma/glp/unidesk/backend/domain/model/Coordenador.java
@@ -1,25 +1,39 @@
 package br.ufma.glp.unidesk.backend.domain.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+
 
 @Entity
+@Table(name = "coordenadores")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Coordenador {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idCoordenador;
+@EqualsAndHashCode(callSuper = true)
+@PrimaryKeyJoinColumn(name = "id_usuario")
+public class Coordenador extends Usuario {
 
+    @Column(nullable = false)
     private Boolean ativo;
+
+    @Column(name = "matricula",nullable = false, unique = true)
+    @NotBlank(message = "Matrícula não pode ser vazia ou conter apenas espaços em branco")
+    @Size(max = 20, message = "Matrícula não pode ter mais de 20 caracteres")
     private String matricula;
 
     @ManyToOne
-    @JoinColumn(name = "id_usuario")
-    private Usuario usuario;
-
-    @ManyToOne
-    @JoinColumn(name = "id_coordenacao")
+    @NotNull(message = "Coordenacao não pode ser nula")
+    @JoinColumn(name = "id_coordenacao", nullable = false)
     private Coordenacao coordenacao;
 }
