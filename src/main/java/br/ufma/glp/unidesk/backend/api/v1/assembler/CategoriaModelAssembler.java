5dc15b69b1897016793a17fa9972aee41e248e0d
Renef <renefricardo30@gmail.com>
2025-07-21 14:40:49 -0300

    feat: adicionar classes disassembler e assembler para Categoria

--- src/main/java/br/ufma/glp/unidesk/backend/api/v1/assembler/CategoriaModelAssembler.java
+++ src/main/java/br/ufma/glp/unidesk/backend/api/v1/assembler/CategoriaModelAssembler.java
@@ -1,8 +1,30 @@
 package br.ufma.glp.unidesk.backend.api.v1.assembler;
 
+import br.ufma.glp.unidesk.backend.api.v1.dto.model.CategoriaModel;
+import br.ufma.glp.unidesk.backend.domain.model.Categoria;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 class CategoriaModelAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public CategoriaModelAssembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public CategoriaModel toModel(Categoria categoria) {
+        return modelMapper.map(categoria, CategoriaModel.class);
+    }
+
+    public List<CategoriaModel> toCollectionModel(List<Categoria> categorias) {
+        return categorias.stream()
+                .map(this::toModel)
+                .collect(Collectors.toList());
+    }
 
 }
