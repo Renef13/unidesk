d40e663ebc5045951a0c2b6ef9d7a3f2edda6ffb
Renef <renefricardo30@gmail.com>
2025-07-21 14:47:51 -0300

    feat: implementar disassemblers para CategoriaCadastro e CategoriaEdicao

--- src/main/java/br/ufma/glp/unidesk/backend/api/v1/assembler/CategoriaCadastroInputDisassembler.java
+++ src/main/java/br/ufma/glp/unidesk/backend/api/v1/assembler/CategoriaCadastroInputDisassembler.java
@@ -1,8 +1,24 @@
 package br.ufma.glp.unidesk.backend.api.v1.assembler;
 
+import br.ufma.glp.unidesk.backend.api.v1.dto.input.CategoriaCadastroInput;
+import br.ufma.glp.unidesk.backend.domain.model.Categoria;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 class CategoriaCadastroInputDisassembler {
+
+    private final ModelMapper modelMapper;
+
+    public Categoria toDoaminObject(CategoriaCadastroInput categoriaCadastroInput) {
+        return modelMapper.map(categoriaCadastroInput, Categoria.class);
+    }
+
+    public void copyToDomainObject(CategoriaCadastroInput categoriaCadastroInput, Categoria categoria) {
+        modelMapper.map(categoriaCadastroInput, categoria);
+    }
+
 
 }
