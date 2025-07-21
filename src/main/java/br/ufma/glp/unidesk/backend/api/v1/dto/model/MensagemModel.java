9dcc4fa64409666c6108abd451aa1e13522f0665
Renef <renefricardo30@gmail.com>
2025-07-21 00:18:00 -0300

    feat: adicionar classes DTO para Mensagem e Usuario

--- src/main/java/br/ufma/glp/unidesk/backend/api/v1/dto/model/MensagemModel.java
+++ src/main/java/br/ufma/glp/unidesk/backend/api/v1/dto/model/MensagemModel.java
@@ -3,7 +3,6 @@ package br.ufma.glp.unidesk.backend.api.v1.dto.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.Instant;
 
 @Data
 @NoArgsConstructor
