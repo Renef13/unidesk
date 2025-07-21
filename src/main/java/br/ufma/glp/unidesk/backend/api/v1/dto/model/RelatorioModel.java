ee83357fb6af1869be4aa7390e016cedcc15d79c
Renef <renefricardo30@gmail.com>
2025-07-20 23:42:56 -0300

    feat: adicionar classes DTO para Prioridade, Relatório e Status

---
+++ src/main/java/br/ufma/glp/unidesk/backend/api/v1/dto/model/RelatorioModel.java
@@ -0,0 +1,18 @@
+package br.ufma.glp.unidesk.backend.api.v1.dto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RelatorioModel {
+
+    private Long idRelatorio;
+    private String conteudo;
+    private LocalDate data;
+    private String tipoRelatorio;
+}
