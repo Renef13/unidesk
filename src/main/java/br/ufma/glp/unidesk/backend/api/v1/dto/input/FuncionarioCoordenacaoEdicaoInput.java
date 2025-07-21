d18b3b70e7e4ffba47a3628b458ddb029b63c1bf
Renef <renefricardo30@gmail.com>
2025-07-21 01:05:50 -0300

    feat: adicionar classes DTO para cadastro e edição de Coordenador

---
+++ src/main/java/br/ufma/glp/unidesk/backend/api/v1/dto/input/FuncionarioCoordenacaoEdicaoInput.java
@@ -0,0 +1,16 @@
+package br.ufma.glp.unidesk.backend.api.v1.dto.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
+public class FuncionarioCoordenacaoEdicaoInput extends FuncionarioCoordenacaoCadastroInput {
+    
+    @EqualsAndHashCode.Include
+    private Long idUsuario;
+}
