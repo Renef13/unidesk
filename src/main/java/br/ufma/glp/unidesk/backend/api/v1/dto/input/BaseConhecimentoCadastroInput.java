678a20833613847d8b725057fc59e8b2164ac25f
Renef <renefricardo30@gmail.com>
2025-07-21 00:58:58 -0300

    feat: adicionar classes DTO para cadastro e edição de BaseConhecimento e Categoria

---
+++ src/main/java/br/ufma/glp/unidesk/backend/api/v1/dto/input/BaseConhecimentoCadastroInput.java
@@ -0,0 +1,23 @@
+package br.ufma.glp.unidesk.backend.api.v1.dto.input;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class BaseConhecimentoCadastroInput {
+
+    private String titulo;
+
+    private String conteudo;
+
+    @NotNull(message = "A categoria do artigo é obrigatória")
+    private Long idCategoria;
+}
