dae704aa67a938d41cca6a3ff613a16c8c226849
Renef <renefricardo30@gmail.com>
2025-07-21 23:01:54 -0300

    feat: adicionar disassemblers para BaseConhecimentoCadastroInput e BaseConhecimentoEdicaoInput

--- src/main/java/br/ufma/glp/unidesk/backend/api/v1/assembler/BaseConhecimentoCadastroInputDisassembler.java
+++ src/main/java/br/ufma/glp/unidesk/backend/api/v1/assembler/BaseConhecimentoCadastroInputDisassembler.java
@@ -1,4 +1,27 @@
 package br.ufma.glp.unidesk.backend.api.v1.assembler;
 
+import br.ufma.glp.unidesk.backend.api.v1.dto.input.BaseConhecimentoCadastroInput;
+import br.ufma.glp.unidesk.backend.domain.model.BaseConhecimento;
+import br.ufma.glp.unidesk.backend.domain.model.Categoria;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class BaseConhecimentoCadastroInputDisassembler {
+
+    private final ModelMapper modelMapper;
+
+    public BaseConhecimento toDomainObject(BaseConhecimentoCadastroInput input) {
+        BaseConhecimento baseConhecimento = modelMapper.map(input, BaseConhecimento.class);
+
+        if (input.getIdCategoria() != null) {
+            Categoria categoria = new Categoria();
+            categoria.setIdCategoria(input.getIdCategoria());
+            baseConhecimento.setCategoria(categoria);
+        }
+
+        return baseConhecimento;
+    }
 }
