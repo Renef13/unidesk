415afa45a7890b8b909c2f0a31c0fbd7b9cf76cf
Renef <renefricardo30@gmail.com>
2025-07-24 01:30:40 -0300

    refactor: aplicar validação de argumentos, uso de lombok e melhorias em TicketService

--- src/main/java/br/ufma/glp/unidesk/backend/domain/service/TicketService.java
+++ src/main/java/br/ufma/glp/unidesk/backend/domain/service/TicketService.java
@@ -1,14 +1,5 @@
 package br.ufma.glp.unidesk.backend.domain.service;
 
-import java.time.Instant;
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
 import br.ufma.glp.unidesk.backend.domain.exception.StatusNaoEncontradoException;
 import br.ufma.glp.unidesk.backend.domain.exception.TicketNaoEncontradoException;
 import br.ufma.glp.unidesk.backend.domain.model.Status;
@@ -16,28 +7,33 @@ import br.ufma.glp.unidesk.backend.domain.model.Ticket;
 import br.ufma.glp.unidesk.backend.domain.model.Usuario;
 import br.ufma.glp.unidesk.backend.domain.repository.StatusRepository;
 import br.ufma.glp.unidesk.backend.domain.repository.TicketRepository;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.Instant;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Validated
 public class TicketService {
 
-    private TicketRepository ticketRepository;
-    private StorageService storageService;
-    private StatusRepository statusRepository;
+    private final TicketRepository ticketRepository;
+    private final StorageService storageService;
+    private final StatusRepository statusRepository;
 
-    public TicketService(TicketRepository ticketRepository, StorageService storageService, StatusRepository statusRepository) {
-        this.ticketRepository = ticketRepository;
-        this.storageService = storageService;
-        this.statusRepository = statusRepository;
-    }
-
-    public Page<Ticket> listarTickets(Usuario usuario, int pageNumber, int pageSize) {
+    public Page<Ticket> listarTickets(@Valid @NotNull Usuario usuario, int pageNumber, int pageSize) {
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
         boolean isAdmin = usuario.getAuthorities().stream().anyMatch(role -> role.getAuthority().contains("ADMIN"));
         if (isAdmin) {
-
             return ticketRepository.findAll(pageable);
         } else {
             return ticketRepository.findByAlunoIdUsuario(usuario.getIdUsuario(), pageable);
@@ -50,51 +46,48 @@ public class TicketService {
             String idFile = storageService.uploadFile(file);
             ticket.setIdFile(idFile);
         }
-
-        ticketRepository.save(ticket);
-        return ticket;
+        return ticketRepository.save(ticket);
     }
 
     @Transactional
-    public String alterarStatusTicket(Ticket ticket, Status status) {
+    public String alterarStatusTicket(@Valid @NotNull Ticket ticket, @Valid @NotNull Status status) {
         ticket.setStatus(status);
+        ticketRepository.save(ticket);
         return "Novo status do ticket: " + status.getNome();
     }
 
     @Transactional
-    public Ticket fecharTicket(Ticket ticket, Long idStatus) {
-        Status novoStatus = statusRepository.findById(idStatus).get();
+    public Ticket fecharTicket(@Valid @NotNull Ticket ticket, @NotNull Long idStatus) {
+        Status novoStatus = statusRepository.findById(idStatus)
+                .orElseThrow(() -> new StatusNaoEncontradoException("Status não encontrado para o id: " + idStatus));
         ticket.setStatus(novoStatus);
-
-        return ticket;
+        return ticketRepository.save(ticket);
     }
 
     public Ticket buscarTicketPorId(@Valid @NotNull(message = "O Id do ticket nao pode ser nulo") Long idTicket) {
-        return ticketRepository.findById(idTicket).orElseThrow(() -> new TicketNaoEncontradoException(idTicket));
+        return ticketRepository.findById(idTicket)
+                .orElseThrow(() -> new TicketNaoEncontradoException(idTicket));
     }
 
     public void filtrarPorCategoria() {
-
+        //TODO: Implementação futura
     }
 
-    public List<Ticket> buscarTicketsPorStatusEspecifico(Usuario usuario, Status status) {
+    public List<Ticket> buscarTicketsPorStatusEspecifico(@Valid @NotNull Usuario usuario, @Valid @NotNull Status status) {
         boolean isAdmin = usuario.getAuthorities().stream().anyMatch(role -> role.getAuthority().contains("ADMIN"));
-        Status statusTipo = statusRepository.findByIdStatus(status.getIdStatus()).orElseThrow(() -> new StatusNaoEncontradoException("Status com tipo " + status.getNome() + " nao encontrado"));
-        if(isAdmin) {
-            return ticketRepository.findByStatus(statusTipo).stream().toList();
-        } 
-        return null;
+        Status statusTipo = statusRepository.findByIdStatus(status.getIdStatus())
+                .orElseThrow(() -> new StatusNaoEncontradoException("Status com tipo " + status.getNome() + " nao encontrado"));
+        if (isAdmin) {
+            return ticketRepository.findByStatus(statusTipo);
+        }
+        return List.of();
     }
 
-    public List<Ticket> buscarTicketsPorPeriodo(Instant dataInicio, Instant dataFim) {
+    public List<Ticket> buscarTicketsPorPeriodo(@NotNull Instant dataInicio, @NotNull Instant dataFim) {
         List<Ticket> ticketsNoPeriodo = ticketRepository.findByDataCriacaoBetween(dataInicio, dataFim);
-        if(ticketsNoPeriodo.isEmpty()) {
+        if (ticketsNoPeriodo.isEmpty()) {
             throw new TicketNaoEncontradoException("Nao encontrado tickets no periodo selecionado");
         }
-
         return ticketsNoPeriodo;
     }
-
-
-
 }
