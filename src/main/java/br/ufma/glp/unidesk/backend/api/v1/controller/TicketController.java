b488d234e18dc5201a9f466bb8671d9227168791
buulzin03 <caio.diniz@discente.ufma.br>
2025-07-30 15:04:47 -0300

    removendo importação MediaType sem uso

--- src/main/java/br/ufma/glp/unidesk/backend/api/v1/controller/TicketController.java
+++ src/main/java/br/ufma/glp/unidesk/backend/api/v1/controller/TicketController.java
@@ -20,7 +20,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
