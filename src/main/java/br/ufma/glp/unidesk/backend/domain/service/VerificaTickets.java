b504df94d60815c5ad7edf0c1fba2666ce4f56c0
buulzin03 <caio.diniz@discente.ufma.br>
2025-08-14 13:49:45 -0300

    Atualizando a schedule para pegar apenas a movimentacao mais recente de cada ticket

--- src/main/java/br/ufma/glp/unidesk/backend/domain/service/VerificaTickets.java
+++ src/main/java/br/ufma/glp/unidesk/backend/domain/service/VerificaTickets.java
@@ -2,7 +2,11 @@ package br.ufma.glp.unidesk.backend.domain.service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import br.ufma.glp.unidesk.backend.domain.model.Status;
 import br.ufma.glp.unidesk.backend.domain.model.Ticket;
+import br.ufma.glp.unidesk.backend.domain.model.TicketMovimentacao;
 import br.ufma.glp.unidesk.backend.domain.repository.TicketMovimentacaoRepository;
 import br.ufma.glp.unidesk.backend.domain.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
@@ -31,8 +36,16 @@ public class VerificaTickets {
         Instant dataAtual = Instant.now();
 
         Instant doisDiasAtras = dataAtual.minus(2, ChronoUnit.DAYS);
+        Map<Ticket, TicketMovimentacao> ultimaMovimentacaoPorTicket = ticketMovimentacaoRepository
+            .findAll().stream().filter(tm -> tm.getDataMovimentacao() != null)
+                .collect(Collectors.groupingBy(
+                    tm -> tm.getTicket(), Collectors.collectingAndThen(
+                        Collectors.maxBy(Comparator.comparing(tm -> tm.getDataMovimentacao())), tm -> tm.get())
+            ));
+
         // pegar os tickets com movimentacao mais antiga que dois dias atras ()
-        List<Ticket> ticketsNaoTratadosNoPrazo = ticketMovimentacaoRepository.findAll().stream().filter(t -> t.getDataMovimentacao() != null && t.getDataMovimentacao().isBefore(doisDiasAtras)).map(t -> t.getTicket()).distinct().toList();
+        List<Ticket> ticketsNaoTratadosNoPrazo = ultimaMovimentacaoPorTicket.values().stream().filter(tm -> tm.getDataMovimentacao().isBefore(doisDiasAtras)).map( tm -> tm.getTicket()).toList();
+
         // para cada ticket desse nao tratado, mudar o status para fechado
         Status statusFechado = statusService.buscarPorNomeOuFalhar("Fechado");
         ticketsNaoTratadosNoPrazo.stream().forEach(t -> t.setStatus(statusFechado));
